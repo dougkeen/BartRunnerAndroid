d3dd0abbc91157ed1694bf7868b17b7f7452f415
Matt Mullins <mmullins@mmlx.us>
2014-11-30 19:24:09 -0800

    Display the notification as a count-down timer.
    This blinks the icon in the notification, though :(

--- wearable/src/main/java/com/dougkeen/bart/WearableDeparture.java
+++ wearable/src/main/java/com/dougkeen/bart/WearableDeparture.java
@@ -10,6 +10,8 @@ import com.google.android.gms.wearable.DataMap;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by mmullins on 11/30/14.
@@ -21,44 +23,67 @@ public class WearableDeparture {
     private String destination;
     private Context context;
     private NotificationManager notificationManager;
+    private Notification.Builder builder;
+    private static Timer timer;
 
     public WearableDeparture(Context ctx) {
         context = ctx;
         notificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent i = new Intent(context, DataListenerService.class)
+                .putExtra("stop_notification", true);
+        PendingIntent pi = PendingIntent.getService(context, 1, i, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder = new Notification.Builder(context)
+                .setSmallIcon(com.dougkeen.bart.R.drawable.ic_launcher)
+                .addAction(R.drawable.ic_launcher, "Cancel", pi)
+                .setOngoing(true)
+        ;
+
     }
 
     public void update(DataMap dataMap) {
         if (dataMap != null) {
             updateFromDataMap(dataMap);
-            updateNotification();
         } else {
             cancelNotification();
+            departure_time = null;
+            destination = null;
         }
     }
 
     public void updateFromDataMap(DataMap dataMap) {
         departure_time = new Date(dataMap.getLong("departure_time"));
         destination = dataMap.getString("destination");
+
+        if (timer == null) {
+            timer = new Timer(true);
+            timer.scheduleAtFixedRate(
+                    new TimerTask() {
+                        @Override
+                        public void run() {
+                            updateNotification();
+                        }
+                    },
+                    0,
+                    1000
+            );
+        }
     }
 
     public void updateNotification() {
-        Intent i = new Intent(context, DataListenerService.class)
-                .putExtra("stop_notification", true)
-                ;
-        PendingIntent pi = PendingIntent.getService(context, 1, i, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        String s = formatter.format(departure_time).toString();
-        Notification.Builder b = new Notification.Builder(context)
-                .setSmallIcon(com.dougkeen.bart.R.drawable.ic_launcher)
-                .setContentTitle("Train to " + destination.toUpperCase()
-                        + " at " + s)
-                .addAction(R.drawable.ic_launcher, "Cancel", pi)
-                .setOngoing(true)
-                ;
-        notificationManager.notify(NOTIFICATION_ID, b.build());
+        long duration = departure_time.getTime() - new Date().getTime();
+        long minutes = duration / 60000;
+        long seconds = ((duration - minutes * 60000) % 60000) / 1000;
+        String s = String.format("%dm%02ds", minutes, seconds);
+        builder.setContentTitle(destination.toUpperCase() + " train in " + s);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 
     public void cancelNotification() {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
         notificationManager.cancel(NOTIFICATION_ID);
     }
 }
